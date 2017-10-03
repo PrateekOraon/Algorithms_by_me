12ec40ef056bb61af9ab88a5771bd56aa4b37d8c
PrateekOraon <oraonprateek251@gmail.com>
2017-10-03 14:21:47 +0530

    prime_numbers added

---
+++ Algorithms_by_me/prime_numbers.cpp
@@ -0,0 +1,21 @@
+//to find all prime number upto A in n*loglogn time
+
+
+vector<int> Solution::sieve(int A) {
+    vector<int> V(A+1,1);
+    vector<int> Z;
+    
+    V[0] = 0;
+    V[1] = 0;
+    for(int i=2;i<=(int)sqrt(A);i++){
+        if(V[i] == 1){
+            for(int j=2;i*j<=A;j++){
+                V[i*j] = 0;
+            }
+        }
+    }
+    for(int i=0;i<V.size();i++){
+        if(V[i]==1) Z.push_back(i);
+    }
+    return Z;
+}
